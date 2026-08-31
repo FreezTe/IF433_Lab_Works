f62e2a8b8b8f5e69913a7f4554857e22df32752f
FreezTe <nonutdread@gmail.com>
2026-08-31 20:09:31 +0700

    week01: (tugas 1) add legacy CurcleCalculator code

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var name : string = "Kotlin"
+    var name : String = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
