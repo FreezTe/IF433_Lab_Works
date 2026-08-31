81bf7d2d9188193a0d5e0fe17c5ef8c02b58a137
FreezTe <nonutdread@gmail.com>
2026-08-31 12:41:32 +0700

    week 01: initial setup and procedural code

--- src/oop_00000090472_RafaelGadingSamoda/week01/GradingSystem.kt
+++ src/oop_00000090472_RafaelGadingSamoda/week01/GradingSystem.kt
@@ -1,2 +1,22 @@
 package oop_00000090472_RafaelGadingSamoda.week01
 
+fun main (){
+    var name : String = "rexYMnvrT"
+    var score : Int = 80
+
+    println("Name :" + name + ", Score: " + score)
+
+//    val name  = "rexYMnvrT"
+//    val score  = 80
+//    val grade = when(score) {
+//        in 90..100 -> "A"
+//        in 80..89 -> "B"
+//        in 70..79 -> "C"
+//        in 60..69 -> "D"
+//        else -> "E"
+//    }
+
+//    println("Grade kamu : $grade\n")
+
+
+}
