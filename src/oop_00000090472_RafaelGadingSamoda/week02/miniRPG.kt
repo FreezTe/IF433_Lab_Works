908e0159206fbf20f20480f9b512c4d45965bc26
FreezTe <nonutdread@gmail.com>
2026-09-06 16:19:07 +0700

    week02: (task 2) create Hero class

--- src/oop_00000090472_RafaelGadingSamoda/week02/miniRPG.kt
+++ src/oop_00000090472_RafaelGadingSamoda/week02/miniRPG.kt
@@ -2,7 +2,26 @@ package oop_00000090472_RafaelGadingSamoda.week02
 import java.util.Scanner
 
 
-class Hero(var ){}
+class Hero(var name: String, var hp : Int = 100, var baseDamage : Int){
+
+    var isAlive : Boolean? = null
+    fun attack(targetName: String){
+        println("$name menyerang $targetName")
+    }
+
+    fun takeDamage(damage : Int){
+        println("$name take $damage")
+        hp -= damage
+        if (hp < 0){
+            hp = 0
+        }
+        if (hp == 0){
+            isAlive = false
+        }else {
+            isAlive = true
+        }
+    }
+}
 
 
 fun miniRPG() {
