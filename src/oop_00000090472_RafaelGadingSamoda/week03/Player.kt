4ae8f8cd9d140ba7b3512b616dfc9ca943c68b7c
FreezTe <nonutdread@gmail.com>
2026-09-15 16:14:30 +0700

    week03: (task 2) create Player class with private xp

--- src/oop_00000090472_RafaelGadingSamoda/week03/Player.kt
+++ src/oop_00000090472_RafaelGadingSamoda/week03/Player.kt
@@ -1,5 +1,24 @@
 package oop_00000090472_RafaelGadingSamoda.week03
 
-fun main(){
+class Player(var username: String) {
 
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addExp(amount: Int) {
+        if (amount < 0) {
+            println("Tidak bisa minus")
+            return
+        }
+
+        val levelSebelum = level
+        xp += amount
+        val levelSesudah = level
+
+        if (levelSesudah > levelSebelum) {
+            println("Level Up! Selamat $username sudah mencapai level $levelSesudah !")
+        }
+    }
 }
